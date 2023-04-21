04e593f1cbaaf88f28239df4c3458a7932bff925
Clockiscool1234 <85726365+Clockiscool1234@users.noreply.github.com>
2023-04-21 20:39:22 +0800

    Create setup7.sh

---
+++ setup7.sh
@@ -0,0 +1,5 @@
+apt-get update
+wget -O w7x64.img https://bit.ly/akuhnetw7X64
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+apt-get install qemu
